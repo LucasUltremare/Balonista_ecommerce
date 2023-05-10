099f6bef7362a66ccbd9025273dcd0eecc65e5c8
Lucas Ultremare <lucas.ultremare@gmail.com>
2023-05-09 21:36:31 -0300

    update

--- checkout.php
+++ checkout.php
@@ -1,96 +1,129 @@
-<?php
-require_once 'mercadopago_config.php'; // Inclua o arquivo de configuração do Mercado Pago
-
+<script src="https://www.mercadopago.com/v2/checkout.js"></script>
+<?php 
 $total = 0;
 $qry = $conn->query("SELECT c.*,p.name,p.price,p.id as pid from `cart` c inner join `inventory` i on i.id=c.inventory_id inner join products p on p.id = i.product_id where c.client_id = ".$_settings->userdata('id'));
-while($row = $qry->fetch_assoc()):
+while($row= $qry->fetch_assoc()):
     $total += $row['price'] * $row['quantity'];
 endwhile;
-
-// Chave pública do Mercado Pago
-$publicKey = 'SEU_PUBLIC_KEY'; // Substitua pela sua chave pública do Mercado Pago
-
-// URLs de redirecionamento após o pagamento
-$successUrl = 'https://yourdomain.com/success'; // URL de sucesso
-$failureUrl = 'https://yourdomain.com/failure'; // URL de falha
-$pendingUrl = 'https://yourdomain.com/pending'; // URL pendente
-
-// Crie a preferência de pagamento no Mercado Pago
-$preferenceData = array(
-    'items' => array(
-        array(
-            'title' => 'Order',
-            'quantity' => 1,
-            'currency_id' => 'BRL',
-            'unit_price' => $total
-        )
-    ),
-    'back_urls' => array(
-        'success' => $successUrl,
-        'failure' => $failureUrl,
-        'pending' => $pendingUrl
-    )
-);
-
-$mp = new MP($publicKey); // Crie uma instância da classe MP com sua chave pública do Mercado Pago
-$preference = $mp->create_preference($preferenceData);
-$paymentUrl = $preference['response']['sandbox_init_point']; // Obtenha a URL de pagamento do Mercado Pago
-
 ?>
-
 <section class="py-5">
     <div class="container">
         <div class="card rounded-0">
-            <div class="card-body">
-                <h3 class="text-center"><b>Checkout</b></h3>
-                <hr class="border-dark">
-                <form action="" id="place_order">
-                    <input type="hidden" name="amount" value="<?php echo $total ?>">
-                    <input type="hidden" name="payment_method" value="cod">
-                    <input type="hidden" name="paid" value="0">
-                    <div class="row row-col-1 justify-content-center">
-                        <div class="col-6">
-                            <div class="form-group col mb-0">
-                                <label for="" class="control-label">Order Type</label>
-                            </div>
-                            <div class="form-group d-flex pl-2">
-                                <div class="custom-control custom-radio">
-                                    <input class="custom-control-input custom-control-input-primary" type="radio" id="customRadio4" name="order_type" value="1" checked="">
-                                    <label for="customRadio4" class="custom-control-label">For Delivery</label>
-                                </div>
-                                <div class="custom-control custom-radio ml-3">
-                                    <input class="custom-control-input custom-control-input-primary custom-control-input-outline" type="radio" id="customRadio5" name="order_type" value="2">
-                                    <label for="customRadio5" class="custom-control-label">
-                                    For Pick up</label>
-                                </div>
-                            </div>
-                            <div class="form-group col address-holder">
-                                <label for="" class="control-label">Delivery Address</label>
-                                <textarea id="" cols="30" rows="3" name="delivery_address" class="form-control" style="resize:none"><?php echo $_settings->userdata('default_delivery_address') ?></textarea>
-                            </div>
-                            <div class="col">
-                                <span><h4><b>Total:</b> <?php echo number_format($total) ?></h4></span>
-                            </div>
-                            <hr>
-                            <div class="col my-3">
-                                <h4 class="text-muted">Payment Method</h4>
-                                <div class="d-flex w-100 justify-content-between">
-                                    <a class="btn btn-flat btn-dark" href="javascript:void(0)" onclick="submitForm('cod')">Cash on Delivery</a>
-                                    <a class="btn btn-flat btn-primary" href="<?php echo $paymentUrl; ?>">Mercado Pago</a>
-                                </div>
+            <div class="card-body"></div>
+            <h3 class="text-center"><b>Checkout</b></h3>
+            <hr class="border-dark">
+            <form action="" id="place_order">
+                <input type="hidden" name="amount" value="<?php echo $total ?>">
+                <input type="hidden" name="payment_method" value="cod">
+                <input type="hidden" name="paid" value="0">
+                <div class="row row-col-1 justify-content-center">
+                    <div class="col-6">
+                    <div class="form-group col mb-0">
+                    <label for="" class="control-label">Order Type</label>
+                    </div>
+                    <div class="form-group d-flex pl-2">
+                        <div class="custom-control custom-radio">
+                          <input class="custom-control-input custom-control-input-primary" type="radio" id="customRadio4" name="order_type" value="1" checked="">
+                          <label for="customRadio4" class="custom-control-label">For Delivery</label>
+                        </div>
+                        <div class="custom-control custom-radio ml-3">
+                          <input class="custom-control-input custom-control-input-primary custom-control-input-outline" type="radio" id="customRadio5" name="order_type" value="2">
+                          <label for="customRadio5" class="custom-control-label">For Pick up</label>
+                        </div>
+                      </div>
+                        <div class="form-group col address-holder">
+                            <label for="" class="control-label">Delivery Address</label>
+                            <textarea id="" cols="30" rows="3" name="delivery_address" class="form-control" style="resize:none"><?php echo $_settings->userdata('default_delivery_address') ?></textarea>
+                        </div>
+                        <div class="col">
+                            <span><h4><b>Total:</b> <?php echo number_format($total) ?></h4></span>
+                        </div>
+                        <hr>
+                        <div class="col my-3">
+                        <h4 class="text-muted">Payment Method</h4>
+                            <div class="d-flex w-100 justify-content-between">
+                                <button class="btn btn-flat btn-dark" id="normal-pay-btn">Cash on Delivery</button>
+                                <button class="btn btn-flat btn-primary" id="mercadopago-button">Mercado Pago</button>
                             </div>
                         </div>
                     </div>
-                </form>
-            </div>
+                </div>
+            </form>
         </div>
     </div>
 </section>
-
 <script>
-    function submitForm(paymentMethod) {
-        $('[name="payment_method"]').val(paymentMethod);
-        $('[name="paid"]').val(1);
-        $('#place_order').submit();
-    }
+function payment_online() {
+    $('[name="payment_method"]').val("Online Payment");
+    $('[name="paid"]').val(1);
+    $('#place_order').submit();
+}
+
+$(function() {
+    $('[name="order_type"]').change(function(){
+        if ($(this).val() == 2) {
+    $('.address-holder').hide('slow');
+    $('#normal-pay-btn').text('Cash on Pickup');
+} else {
+    $('.address-holder').show('slow');
+    $('#normal-pay-btn').text('Cash on Delivery');
+}
+});
+
+$('#place_order').submit(function(e) {
+    e.preventDefault();
+    start_loader();
+    $.ajax({
+        url: 'classes/Master.php?f=place_order',
+        method: 'POST',
+        data: $(this).serialize(),
+        dataType: "json",
+        error: err => {
+            console.log(err);
+            alert_toast("An error occurred", "error");
+            end_loader();
+        },
+        success: function(resp) {
+            if (!!resp.status && resp.status == 'success') {
+                alert_toast("Order successfully placed.", "success");
+                setTimeout(function() {
+                    location.replace('./');
+                }, 2000);
+            } else {
+                console.log(resp);
+                alert_toast("An error occurred", "error");
+                end_loader();
+            }
+        }
+    });
+});
+
+// Mercado Pago integration
+$('#mercadopago-button').click(function() {
+    var amount = <?php echo $total; ?>;
+
+    // Initialize Mercado Pago Checkout
+    var checkout = new MercadoPago.Checkout({
+        publicKey: 'APP_USR-c23796fb-6589-4e5d-8175-ba8e246e43bd' // Replace with your Mercado Pago Public Key
+    });
+
+    // Create the preference object
+    var preference = {
+        items: [{
+            title: 'Order',
+            quantity: 1,
+            currency_id: 'PHP',
+            unit_price: amount
+        }],
+        back_urls: {
+            success: 'https://yourdomain.com/success',
+            failure: 'https://yourdomain.com/failure',
+            pending: 'https://yourdomain.com/pending'
+        }
+    };
+
+    // Open the Mercado Pago Checkout
+    checkout.open(preference);
+});
+});
 </script>
---
+++ mercadopago_config.php
@@ -0,0 +1,12 @@
+<?php
+
+// Chaves de acesso do Mercado Pago
+const MP_PUBLIC_KEY = 'APP_USR-c23796fb-6589-4e5d-8175-ba8e246e43bd';
+const MP_ACCESS_TOKEN = 'APP_USR-1632996872015800-012421-2aa47597c1edc53f0e09bffa55a233cc-1158040355';
+
+// URLs de redirecionamento
+define('SUCCESS_URL', 'https://seusite.com/checkout/success.php');
+define('FAILURE_URL', 'https://seusite.com/checkout/failure.php');
+define('PENDING_URL', 'https://seusite.com/checkout/pending.php');
+
+?>
